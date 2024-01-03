eaa2b9a7992b9b8c3496594a7b6684ba45ff5596
Tayyab-Ahmad-44 <tayyabuetm24@gmail.com>
2024-01-03 18:10:39 +0500

    Simple GUI Form

---
+++ 01 Form/Code/__astcache/@c@@users@tayya@onedrive@documentos@embarcadero@studio@projects/d@@programing@c++@gui@form.cpp
@@ -0,0 +1,31 @@
+﻿//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#pragma hdrstop
+
+#include "Form.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.fmx"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::AddTaskButtonClick(TObject *Sender)
+{
+	String taskText = TaskEdit->Text;
+	TasksListBox->Items->Add(taskText);
+
+	TaskEdit->Text = "";
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::TasksListBoxItemClick(TCustomListBox * const Sender, TListBoxItem * const Item)
+
+{
+	int index = Item->Index;
+	TasksListBox->Items->Delete(index);
+}
+//---------------------------------------------------------------------------
