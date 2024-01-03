2f0944690a65bbd6ff94ab1fc35cba653b5ccc84
Tayyab-Ahmad-44 <tayyabuetm24@gmail.com>
2024-01-03 18:03:40 +0500

    Simple Form Desktop Application

---
+++ 01 Form/Code/__astcache/@c@@users@tayya@onedrive@documentos@embarcadero@studio@projects/d@@programing@c++@gui@form.h
@@ -0,0 +1,34 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef FormH
+#define FormH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.Layouts.hpp>
+#include <FMX.ListBox.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *Label1;
+	TEdit *TaskEdit;
+	TButton *AddTaskButton;
+	TListBox *TasksListBox;
+	void __fastcall AddTaskButtonClick(TObject *Sender);
+	void __fastcall TasksListBoxItemClick(TCustomListBox * const Sender, TListBoxItem * const Item);
+	void __fastcall TaskEditEnter(TObject *Sender);
+
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
